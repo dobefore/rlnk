cd7b6fcca841ac913fc0d359d0fc75c703bf8380
dobefore <1432338032@qq.com>
2021-08-04 09:38:46 +0800

    add_guard_to_ensure_path_exist

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@
 //! let target=r"C:\Users\Admin\Desktop\new aa\qiuqiu.exe";
 //! let lnk=r"C:\Users\Admin\Desktop\qiuqiu.lnk";
 //! let mut sl=ShellLink::new();
-//! sl.set_target(target);
-//! sl.create_lnk(lnk);
+//! sl.create_lnk(target,lnk).unwap();
 //! ```
 use std::{
     fs,
@@ -23,7 +22,6 @@ pub struct ShellLink {
 
 impl ShellLink {
     /// create an instance of struct ShellLink,same as default()
-    #[cfg(windows)]
     pub fn new() -> Self {
         ShellLink::default()
     }
@@ -42,8 +40,7 @@ impl ShellLink {
     /// let target=r"C:\Users\Admin\Desktop\new aa\qiuqiu.exe";
     /// sl.set_target(target);
     ///```
-    #[cfg(windows)]
-    pub fn set_target<P: AsRef<str>>(&mut self, file_path: P) {
+    fn set_target<P: AsRef<str>>(&mut self, file_path: P) {
         self.target = file_path.as_ref().to_owned();
         let working_dir = Path::new(file_path.as_ref())
             .parent()
@@ -52,12 +49,30 @@ impl ShellLink {
             .unwrap();
         self.working_dir = working_dir.to_owned();
     }
-    ///create lnk file(.lnk) of target
+
+    ///create lnk file(.lnk) of target .
+    ///file_target and lnk_path are required .
     ///
-    /// return error if command(shortcut.exe) encounter false filepath
-    #[cfg(windows)]
-    pub fn create_lnk<P: AsRef<str>>(&self, lnk_path: P) -> io::Result<()> {
+    /// panic if wrong filepath is specified
+    pub fn create_lnk<P: AsRef<str>, Q: AsRef<str>>(
+        &mut self,
+        target_path: Q,
+        lnk_path: P,
+    ) -> io::Result<()> {
+        assert!(path_exists(target_path.as_ref()), "target path not exist");
+        assert!(
+            path_exists(
+                Path::new(lnk_path.as_ref())
+                    .parent()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+            ),
+            "lnk parent_dir not exist"
+        );
+
         let shortcut_exe_path = write_exe_to_temp().unwrap();
+        self.set_target(target_path.as_ref());
         let tgt = format!("/t:{}", self.target);
         let work_dir = format!("/w:{}", self.working_dir);
         let lnk = format!("/f:{}", lnk_path.as_ref());
@@ -70,21 +85,27 @@ impl ShellLink {
         } else {
             Err(io::Error::new(
                 io::ErrorKind::Other,
-                "command function abnormally",
+                "sc cmd function abnormally",
             ))
         }
     }
 }
+fn path_exists(path: &str) -> bool {
+    Path::new(path).exists()
+}
 fn is_scexe_in_temp(temp_path: &str) -> bool {
-    if Path::new(temp_path).exists() {
+    if path_exists(temp_path) {
         return true;
     }
     false
 }
 fn write_exe_to_temp() -> Result<String> {
-    let shortcut_exe = include_bytes!("bin/Shortcut.exe");
     let temp = r"C:\Windows\Temp\Shortcut.exe";
-    fs::write(temp, shortcut_exe).unwrap();
+    if !is_scexe_in_temp(temp) {
+        let shortcut_exe = include_bytes!("bin/Shortcut.exe");
+        fs::write(temp, shortcut_exe).unwrap();
+    }
+
     Ok(temp.to_owned())
 }
 
@@ -93,6 +114,5 @@ fn test_create_lnk() {
     let target = r"C:\Users\Admin\Desktop\new aa\sdr-Cleaner_win.exe";
     let lnk = r"C:\Users\Admin\Desktop\new aa\sdr-Cleaner_win.lnk";
     let mut sl = ShellLink::new();
-    sl.set_target(target);
-    sl.create_lnk(lnk).unwrap();
+    sl.create_lnk(target, lnk).unwrap();
 }
